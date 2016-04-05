8693168b077ca3b40dd2f5f4199394e81bd6515d
David Conley <David@Davids-MacBook-Pro.local>
2016-04-05 19:26:32 -0400

    game works with two users. Now adding AI

---
+++ sticks.py
@@ -0,0 +1,160 @@
+import sys
+import random
+from sticks_ai import *
+import pickle
+
+def which_game():
+    print('\n    Game Options:'
+    '\n \nPlay against a friend (1)'
+    '\nPlay against the computer (2)'
+    '\nComputer plays against the computer (3)')
+    while True:
+        game_choice = input('Which option do you take (1, 2, or 3)?')
+        try:
+            game_choice = int(game_choice)
+            if game_choice == 1:
+                return 1
+            if game_choice == 2:
+                return 2
+            if game_choice == 3:
+                return 3
+            else:
+                print('Please enter 1, 2, or 3')
+        except:
+            print('Please enter 1, 2, or 3')
+
+
+
+def initial_number_of_sticks():
+    while True:
+        user_input_num_of_sticks = input('How many sticks are there on the table initially (10-100)?')
+        try:
+            user_input_num_of_sticks = int(user_input_num_of_sticks)
+            if 10 <= user_input_num_of_sticks <= 100:
+                return user_input_num_of_sticks
+            else:
+                print('Please enter a number between 10 and 100')
+        except:
+            print('Please enter a number between 10 and 100')
+
+
+# def create_list_ten_to_num_of_sticks(number_of_start_sticks):
+#     return list(range(10,number_of_start_sticks))
+
+def user_name_two(game_choice):
+    if game_choice == 1:
+        user_two_name = input('Please enter player 2\'s name: ')
+        return user_two_name
+    elif game_choice == 2:
+        user_two_name = 'AI BOT'
+        return user_two_name
+    elif game_choice == 3:
+        print('Skynet Initializing...')
+
+
+
+
+def number_of_sticks_to_remove(player, number_of_sticks, hat_dict):
+    if player == 'AI BOT':
+        number_to_remove = ai_bot_turn(number_of_sticks, hat_dict)
+        number_of_sticks = number_of_sticks - number_to_remove
+        print('AI Bot removed {} sticks'.format(number_to_remove))
+        return number_of_sticks
+    while True:
+        try:
+            number_to_remove = int(input('{}: How many sticks do you take (1-3)?'.format(player)))
+            if 1 <= number_to_remove <=3:
+                number_of_sticks = number_of_sticks - number_to_remove
+                return number_of_sticks
+            else:
+                print('Please enter 1, 2, or 3')
+        except:
+            print('Please enter 1, 2, or 3')
+
+def which_users_turn(count_of_turns, user_one, user_two):
+    if count_of_turns % 2 == 1:
+        return user_one
+    elif count_of_turns % 2 == 0:
+        return user_two
+
+def is_game_over(number_of_sticks):
+    if number_of_sticks <= 0:
+        return True
+
+
+def play_again():
+    return input('Would you like to play again? Y/N: ')
+
+
+
+
+
+
+
+
+
+def main():
+    good_guesses = {}
+    count_of_turns = 1
+    print('\nWelcome to the Game of Sticks!')
+    game_choice = which_game()
+    user_one = input('Please enter player 1\'s name: ')
+
+    user_two = user_name_two(game_choice)
+
+    print('player1: {} \nplayer2: {}\n'.format(user_one, user_two))
+
+    number_of_sticks = initial_number_of_sticks()
+
+    with open('hat_dict_pickle.pickle', 'rb') as handle:
+      hat_dict = pickle.load(handle)
+
+    print(hat_dict)
+
+
+    while number_of_sticks > 0:
+
+        print('\nThere are {} sticks on the table\n'.format(number_of_sticks))
+
+        player = which_users_turn(count_of_turns, user_one, user_two)
+
+        #Does player = bot
+
+        number_of_sticks = number_of_sticks_to_remove(player, number_of_sticks, hat_dict)
+
+        game_over = is_game_over(number_of_sticks)
+
+        # did_ai_win == did_ai_win_or_lose(player, game_over)
+
+        # if did_ai_win == True:
+
+
+
+        count_of_turns += 1
+
+        if game_over == True:
+            if play_again() == 'y':
+                main()
+            break
+
+    with open('hat_dict_pickle.pickle', 'wb') as output:
+      pickle.dump(hat_dict, output, -1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
