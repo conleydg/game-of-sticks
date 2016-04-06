ae3c65563e6bea01d2efae60916850e1a4f28bd0
David Conley <David@Davids-MacBook-Pro.local>
2016-04-05 21:37:38 -0400

    just a few functions

--- sticks_ai.py
+++ sticks_ai.py
@@ -1,28 +1,30 @@
 import random
 
 
-def ai_bot_create_dict(number_of_sticks):
-    hat_dict = {}
-    stick_count = 0
-    while stick_count < number_of_sticks:
-        hat_dict[stick_count] = [1, 2, 3]
-        stick_count +=1
-    return hat_dict
+
 
 
 def did_ai_win_or_lose(player, game_over):
-    if player == 'AI Bot' and game_over == True:
-        print('AI Bot lost!')
-        False
-    elif player != 'AI Bot' and game_over == True:
+    if player == 'A.I. Bot' and game_over == True:
+        print('A.I. Bot lost!')
+        return False
+    elif player != 'A.I. Bot' and game_over == True:
         print('{} lost!'.format(player))
-        True
+        return True
 
 
-def ai_bot_turn(number_of_sticks, hat_dict, good_guesses):
-    this_turns_guess = random.choice(hat_dict[number_of_sticks])
-    good_guesses[number_of_sticks] = this_turns_guess
-    return this_turns_guess, good_guesses
+def ai_bot_turn(number_of_sticks, hat_dict):
+    this_turns_guess = int(random.choice(hat_dict[number_of_sticks]))
+    return this_turns_guess
+
+
+def if_ai_won_update_hat_dict(did_ai_win, good_guesses, hat_dict):
+    if did_ai_win == True:
+        for key, value in good_guesses.items():
+            hat_dict[key].append(value)
+        return hat_dict
+
+
 
 # def update_ai_hat_dict(good_guesses, hat_dict):
 #     for sticks in good_guesses:
